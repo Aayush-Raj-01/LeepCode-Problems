0d74f1f009c9b7318e744d72d049b0604532ccfb
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-01-07 23:51:36 +0530

    Time: 3 ms (53.76%), Space: 8.4 MB (71.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -1,17 +1,22 @@
 #include <limits.h>
 int reverse(int x){
-    int re=0,n;
+    int yx=0,re=0,n=0;
   
     while(x!=0){
         n = x%10;
-      
+        if(yx==0){
+            if(n==0)
+            x = x/10;
+        }
+        else{
             if(re>INT_MAX/10 || re <INT_MIN/10){
                 return 0;
             }
 
             re = n + re*10;
             x = x/10;
-       
+        }
+        yx++;
     }
     return re;
 
