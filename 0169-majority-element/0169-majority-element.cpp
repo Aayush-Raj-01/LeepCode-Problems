f7afe3b4accf40390456188ddfe9a9f71b6131e2
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-01-25 21:52:52 +0530

    Time: 4 ms (19.26%), Space: 28 MB (86.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,8 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        n = (n/2) ;
-        int ans = nums[n];
-        return ans;
+        return nums[n/2];
     }
 };
