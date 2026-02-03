b5bae5fec70c1aa2ae8b97df1dfafc2d0fa68cad
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-02-03 20:08:11 +0530

    Time: 0 ms (100%), Space: 20.3 MB (83.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int end=0,far=0,small=0;
-        for(int i =0;i<nums.size();i++){
-            far = max(far,i+nums[i]);
-            if(i==end){
-                small +=1;
-                end=far;
+
+        int numSize = nums.size();
+            if(numSize <= 1) {
+        return 0;
+    }
+    
+    int jumps = 0;
+    int CurrentEnd = 0;
+    int next = 0;
+    
+    for(int i = 0; i < numSize - 1; i++) {
+        next = max(next, i + nums[i]);
+        
+        if(i == CurrentEnd) {
+            jumps++;
+            CurrentEnd = next;
+            
+            if(CurrentEnd >= numSize - 1) {
+                break;
             }
         }
-        return small-1;
+    }
+    
+    return jumps;
     }
 };  
