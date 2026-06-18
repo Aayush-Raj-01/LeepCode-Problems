5392928f2d3e26be1e62664fb40b95667e1cf5cf
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-06-18 11:12:26 +0530

    Time: 77 ms (52.26%), Space: 3.9 MB (23.43%) - LeetHub

--- 0192-word-frequency/0192-word-frequency.sh
+++ 0192-word-frequency/0192-word-frequency.sh
@@ -1,4 +1,4 @@
 # Read from the file words.txt and output the word frequency list to stdout.
 #!/bin/bash
 
-cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -rn | awk '{print $2, $1}'
+tr -s '[:space:]' '\n' < words.txt | sort | uniq -c | sort -nr | awk '{print $2, $1}'
