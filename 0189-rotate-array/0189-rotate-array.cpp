adcb4293b6cd1db7c84f9fc301e38df946d88a3e
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-07-04 19:25:10 +0530

    Time: 0 ms (100%), Space: 29.7 MB (44.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        if(n == 0 || n == 1) return;  // Handle edge case
         
-        k %= n;  // Handle k > n
-        if(k == 0) return;  // No rotation needed
+        k %= n;
         
 
 
