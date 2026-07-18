6bb1ab11fe1e600b72d797e3b570f10aff8dc01c
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-07-18 22:17:09 +0530

    Time: 11 ms (59.83%), Space: 46.3 MB (73.97%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int calculate(String s) {
+        int result=0;
+        int sign = 1;
+        int number=0;
+        Stack<Integer> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stack.push(result);
+                stack.push(sign);
+                result = 0 ;
+                sign = +1;
+            }
+            else if (c==')'){
+                result += number * sign;
+                sign = stack.peek();
+                stack.pop();
+                number = stack.peek();
+                stack.pop();
+                result = number + result * sign;
+                number = 0;
+            }
+            else if(c=='+'){
+                result = result + number * sign;
+                number = 0;
+                sign = 1;
+            }
+            else if(c == '-'){
+                result = result + number * sign;
+                number = 0;
+                sign = -1;
+            }
+            else if(c != ' '){
+                number = number*10 + (c - '0');
+            }
+        }
+        result += number * sign;
+        return result;
+        
+    }
+}
