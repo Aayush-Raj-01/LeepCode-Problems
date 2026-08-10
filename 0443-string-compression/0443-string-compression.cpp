2a3927cad096b7e161436fba255df0358662646a
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-08-10 11:38:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (95.26%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        char t = chars[0], write;
+        int count = 0, totalcount = 0, posi = 0;
+        for (char read : chars) {
+            if (t == read) {
+                count++;
+            } else {
+                chars[posi] = write;
+                if (chars.size() > posi + 1 && count > 1) {
+                    if (count <= 9) {
+                        chars[posi + 1] = count + '0';
+                        totalcount += 2;
+                        posi += 2;
+                    } else {
+                        string c = to_string(count);
+                        for (int i = 1; i <= c.size(); i++) {
+                            chars[posi + i] = c[i - 1];
+                        }
+                        totalcount += c.size() + 1;
+                        posi += c.size() + 1;
+                    }
+                } else {
+                    posi += 1;
+                    totalcount += 1;
+                }
+                count = 1;
+            }
+            t = read;
+            write = read;
+        }
+        chars[posi] = t;
+
+        if (count > 1) {
+            if (count <= 9) {
+                chars[posi + 1] = count + '0';
+                totalcount += 2;
+                posi += 2;
+            } else {
+                string c = to_string(count);
+                for (int i = 1; i <= c.size(); i++) {
+                    chars[posi + i] = c[i - 1];
+                }
+                totalcount += c.size() + 1;
+                posi += c.size() + 1;
+            }
+        } else {
+            totalcount += 1;
+        }
+        return totalcount;
+    }
+};
