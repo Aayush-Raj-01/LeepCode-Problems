ee75d8cd4a4aceb577c9157de65d63f79f694a25
Aayush Raj <92618098+Aayush-Raj-01@users.noreply.github.com>
2026-08-23 00:40:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.55%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool checkDivisibility(int n) {
-        // digit sum
         int s = n,sum=0,product=1;
         while(s){
             sum+= s%10;
-            s = s /10;
-        }
-        // digit Product
-        s =n;
-        while(s){
             product *= s%10;
             s = s /10;
         }
